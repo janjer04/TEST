a5ad6920e26568de69878113036be53f385fe35f
janjer04 <janetjoshlinait2016@gmail.com>
2018-01-25 23:51:50 +0530

    child2 added

--- src/org/test/Base.java
+++ src/org/test/Base.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("base");
 	System.out.println("adding child");
 	System.out.println("editing child 1");
+	System.out.println("adding child 2");
 }
 }
